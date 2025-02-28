33f499e7525aa818f106fb36bac8fb222ff47b42
Elvira <elveechernenko@yandex.ru>
2025-02-28 14:46:26 +0300

    feat: add styles for keys

--- src/components/PianoKeyItem/PianoKeyItem.tsx
+++ src/components/PianoKeyItem/PianoKeyItem.tsx
@@ -21,18 +21,15 @@ export const PianoKeyItem = (props: PianoKeyItemProps) => {
   const keyRef = useRef<HTMLAudioElement | null>(null);
   const [ isKeyUp, setKeyUp ] = useState(true);
   const [ isBlocked, setBlocked ] = useState(false);
+  const [ isActive, setActive ] = useState(false);
 
   const onPlayClick = (evt: SyntheticEvent<EventTarget>) => {
-    console.log('target', evt.target);
-    console.log('currentTarget', evt.currentTarget);
     if (!(evt.currentTarget instanceof HTMLButtonElement)) {
       return;
     }
 
-    console.log(evt.currentTarget.dataset.note);
     if (evt.currentTarget.dataset.note === noteName) {
       if (keyRef.current) {
-        console.log(keyRef.current);
         keyRef.current.currentTime = 0.0;
         keyRef.current.play();
       }
@@ -40,27 +37,24 @@ export const PianoKeyItem = (props: PianoKeyItemProps) => {
   };
 
   const onPlayKeyUp = (evt: KeyboardEvent) => {
-    if (evt.key === keyCode && keyRef.current) {
-      console.log('keyup');
+    if (evt.key.toLocaleLowerCase() === keyCode.toLocaleLowerCase() && keyRef.current) {
+      setActive(false);
       setKeyUp(true);
     }
   };
 
   const onPlayKeyDown = (evt: KeyboardEvent) => {
-    console.log('keydown', isBlocked);
-    console.log(evt.key);
 
     if (!isKeyUp || isBlocked) {
-      console.log(isBlocked, 'blocked');
       return;
     }
 
     if (evt.key === keyCode && keyRef.current) {
       keyRef.current.currentTime = 0.0;
       keyRef.current.play();
+      setActive(true);
       setKeyUp(false);
       setBlocked(true);
-      console.log(keyRef.current.duration, isBlocked);
 
       setTimeout(() => {
         setBlocked(false);
@@ -100,7 +94,12 @@ export const PianoKeyItem = (props: PianoKeyItemProps) => {
     <li className={cn(cls.item, className)}>
       <audio ref={keyRef} src={`../../../src/assets/sounds/${file}`}></audio>
       <button
-        className={cn(cls.button, { [`${cls['button--black']}`]: isBlack })}
+        className={cn(
+          cls.button,
+          { [`${cls['button--black']}`]: isBlack },
+          { [`${cls['button--active']}`]: isActive },
+          { [`${cls['button--black-active']}`]: isActive && isBlack }
+        )}
         data-note={noteName}
         type='button'
         onClick={onPlayClick}
